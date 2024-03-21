4f846ffc1937d575c021d2d38d93c01144633807
12_Khuất Duy Đức_0068 <kdduc.dhkl17a2hn@sv.uneti.edu.vn>
2024-03-21 15:35:04 +0700

    Create bai4.py

---
+++ lab2/bai4.py
@@ -0,0 +1,9 @@
+n=float(input("Nhập vào điểm kiểm tra : "))
+if 0<=n<5:
+    print("Điểm kém")
+elif 5<=n<7:
+    print("Điểm trung bình")
+elif 7<=n<8.5:
+    print("Điểm Khá")
+elif 8.5<=n<=10:
+    print("Điểm Tốt")
