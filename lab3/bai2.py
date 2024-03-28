546baec13f3d6ae24dab5c4f68605fd95ea7047d
12_Khuất Duy Đức_0068 <kdduc.dhkl17a2hn@sv.uneti.edu.vn>
2024-03-28 21:01:37 +0700

    Create bai2.py

---
+++ lab3/bai2.py
@@ -0,0 +1,12 @@
+#a
+tong=0
+for i in range(2000,4000):
+    if i%7==0 and i%5!=0:
+        tong+=i
+print(tong)
+#b
+ta=0
+for i in range(500,1000):
+    if i%4==0 and i%6!=0:
+        ta+=i
+print(ta)
