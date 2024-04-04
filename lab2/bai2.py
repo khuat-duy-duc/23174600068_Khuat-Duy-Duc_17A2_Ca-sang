bde900c3445c61cab5cd33b6c9b16beaff9613d7
12_Khuất Duy Đức_0068 <kdduc.dhkl17a2hn@sv.uneti.edu.vn>
2024-04-04 21:56:21 +0700

    Update bai2.py

--- lab2/bai2.py
+++ lab2/bai2.py
@@ -1,6 +1,7 @@
 n=int(input("Nhập vào số nguyên : "))
 deptrai=n//1000
-if deptrai==0:
+duc=deptrai%10
+if duc==0:
     print("0")
 else:
-    print(f"Chữ số hàng nghìn là : {deptrai}")
+    print(f"Chữ số hàng nghìn là : {duc}")
